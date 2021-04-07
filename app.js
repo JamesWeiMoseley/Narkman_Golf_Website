bd13352f5fbd92b8ba6a7bdebeed0c7e21798b82
James Moseley <jamesmoseley@jamess-mbp.lan>
2021-04-06 23:08:46 -0700

    added twilio

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
+require('dotenv').config();
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,8 +17,10 @@ app.use(session({
     }
 }))
 
+//connect to mongodb
 mongoose.connect("mongodb+srv://james-admin:jamesmoseley@cluster0.ucovw.mongodb.net/mongo-test-site", { useUnifiedTopology: true }, { useNewUrlParser: true })
 
+//schema that is sent to mongodb
 const notesSchema= {
     n: String, 
     email: String,
@@ -26,12 +29,12 @@ const notesSchema= {
 
 const Note = mongoose.model("Note", notesSchema);
 
-//alert
-app.use((req, res, next)=> {
-    res.locals.message = req.session.message
-    delete req.session.message
-    next()
-})
+//pop up alert
+// app.use((req, res, next)=> {
+//     res.locals.message = req.session.message
+//     delete req.session.message
+//     next()
+// })
 
 app.post("/", function(req, res) {
     let newNote = new Note({
@@ -40,18 +43,12 @@ app.post("/", function(req, res) {
         phone: req.body.phone
     });
     newNote.save();
-    req.session.message = {
-        message: 'Thanks for signing up! We will get back to you shortly!'
-    }
-    res.redirect("/signup");
+    // req.session.message = {
+    //     message: 'Thanks for signing up! We will get back to you shortly!'
+    // }
+    res.redirect("/alert");
 })
 
-
-
-// app.post('/', (req, res)=> {
-    
-//     res.redirect('/')
-// })
 
 const handlebars = require('express-handlebars');
 
@@ -62,8 +59,10 @@ app.engine('hbs', handlebars({
     defaultLayout: 'index'
 }));
 
+//allow access to public folder
 app.use(express.static('public'));
 
+//renders pages in get req
 app.get('/', (req, res)=> {
     res.render('main');
 });
@@ -88,3 +87,33 @@ app.get('/snow', (req, res)=> {
 app.listen(port, () => {
     console.log("App is running on port 5000")
 })
+
+
+
+// twilio
+
+const accountSid=process.env.TWILIO_ACCOUNT_SID;
+const authToken=process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+const num = '+16263213720'
+
+app.get('/alert', (req, res) =>  {
+    textMessage();
+    res.send(`
+    <div style="text-align: center; padding: 4rem">
+        <h1>Thanks for signing up.  An instructor will get back to you shortly.</h1>
+        <a href="/signup">Go Back</a>
+        </div>
+    `);
+})
+
+function textMessage() {
+    client.messages.create({
+            body: 'Do not respond to this message...Thanks for signing up! Our instructors will respond soon!',
+            from: '+12075172239',
+            to: num
+        })
+        .then(message=>console.log(message))
+        .catch((err)=>console.log(err));
+}
